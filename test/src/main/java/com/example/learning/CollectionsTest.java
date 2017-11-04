bfae133f54ff8e8d24d1b87720ddc8f4c7c1452d
mgopinathan <mgopinathan@avaya.com>
2017-11-04 19:47:04 +0530

    Collections Sorting Java8 Example

---
+++ test/src/main/java/com/example/learning/CollectionsTest.java
@@ -0,0 +1,26 @@
+package com.example.learning;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> names = new ArrayList<>();
+		names.add("Sansa Stark");
+		names.add("Brann Stark");
+		names.add("Rob Stark");
+		names.add("Arya Stark");
+		names.add("Nerd Stark");
+		names.add("Catalyn Stark");
+		
+		System.out.println("Before: " + names);
+		
+		Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
+		
+		System.out.println("After: " + names);
+	}
+
+}

b893df591a61fdcde83130c9faf4fe490d679c2b
mgopinathan <mgopinathan@avaya.com>
2017-11-16 18:06:55 +0530

    Cart Service

--- cart-rest-service/src/main/java/com/bootexample/cart/service/CartService.java
+++ cart-rest-service/src/main/java/com/bootexample/cart/service/CartService.java
@@ -1,7 +1,6 @@
 package com.bootexample.cart.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;

128702e7540f0e0503cb91d1fbaacaea0e370216
mgopinathan <mgopinathan@avaya.com>
2017-12-05 17:54:08 +0530

    Hacker Rank algorithms

--- test/src/main/java/com/example/interview/basics/Hacker1.java
+++ test/src/main/java/com/example/interview/basics/Hacker1.java
@@ -6,10 +6,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 
 public class Hacker1 {
@@ -18,13 +21,238 @@ public class Hacker1 {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) throws Exception {
+	static long getWays(long goal, long[] coins){
+
+        long[] solutionsTable = new long[(int)goal+1];
+
+        solutionsTable[0] = (long)1;
+
+        int step = 0;
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = (int)coins[i]; j <= goal; j++) {
+            	int count = j - (int) coins[i];
+                solutionsTable[j] += solutionsTable[count];
+
+                System.out.println("---------------" + " step " + step + " ----------------");
+                System.out.println("ultimate goal: " + goal);
+                System.out.println("coins available: " + Arrays.toString(coins));
+                System.out.println("current coin: " + coins[i]);
+                System.out.println("current goal: " + j + "\n");
+
+                System.out.println(Arrays.toString(solutionsTable) + "\n");
+
+                step++;
+            }
+        }
+
+        return solutionsTable[(int)goal];
+
+    }
+
+	static int minimumAbsoluteDifference(int n, int[] arr) {
+        // Complete this function
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < n-1; i++)
+        {
+        	//System.out.println(String.format("%d - %d = %d", arr[i], arr[i+1], arr[i]-arr[i+1]));
+            int currentMin = Math.abs(arr[i]-arr[i+1]);
+            min = Math.min(min, currentMin);
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        int result = minimumAbsoluteDifference(n, arr);
+        System.out.println(result);
+        in.close();
+    }
+	
+	static int camelCase(String s) {
+		int count = 0;
 		
-		testMain();
+		for (int i=0; i<s.length(); i++) {
+			char ch = s.charAt(i);
+			if (Character.isUpperCase(ch)) {
+				//System.out.println(" " + ch);
+				count++;
+			}
+		}
+		
+		return count+1;
 	}
 	
-	public static void tst() {
-	}
+	static int migratoryBirds(int n, int[] ar) {
+        // Complete this function
+		Map<Integer, Integer> typeCounts = new HashMap<>();
+		for (int i = 1; i<=5; i++) {
+			typeCounts.put(i, 0);
+		}
+		
+		int max = 0;
+		for (int i=0;i<n;i++) {
+			for (Integer key : typeCounts.keySet()) {
+				if (ar[i] == key) {
+					int count = typeCounts.get(key) + 1;
+					typeCounts.put(key, count);
+					System.out.println(String.format("ar[%d], %d == %d +++ %d", i, ar[i], key, count));
+				}
+			}
+		}
+		
+		int val = 0;
+		for (Integer key : typeCounts.keySet()) {
+			if (typeCounts.get(key) > max) {
+				max = typeCounts.get(key);
+				val = key;
+			}
+			System.out.println(key + "::" + typeCounts.get(key));
+		}
+		
+		return val;
+    }
+	
+	static int divisibleSumPairs(int n, int k, int[] ar) {
+        // Complete this function
+        int counter = 0;
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<n;j++) {
+            	//if (i > j) System.out.println(String.format("ar[%d] = %d; ar[%d] = %d", i, ar[i], j, ar[j]));
+            	
+                if ( (i > j) && ((ar[i] + ar[j]) % k == 0) ) {
+                	//System.out.println(String.format("(%d, %d)", ar[i], ar[j]));
+                	counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+	
+	static String super_reduced_string(String s){
+        // Complete this function	
+		
+		char c[] = s.toCharArray();
+		System.out.println(c.length);
+		Arrays.sort(c);
+		System.out.println(c.length);
+		
+		String back = Arrays.toString(c);
+		System.out.println(back.toString());
+		
+		int i = 0;
+		while (i < c.length) {
+			System.out.println(" i = " + i + "; back.len = " + c.length);
+			if ((i < c.length) && (c[i] == c[i+1]) ) {
+				back = back.substring(i+1);
+			}
+			i++;
+		}
+		
+        return "Empty String";
+    }
+	
+	static int betweenTwoSets(int[] a, int[] b) {
+        // Complete this function         
+        int xMin = 1, xMax = 101;
+        int sum = 0;
+        
+        intCheck:
+        for (int i=xMin; i<=xMax; i += xMin) {
+
+            for (int num : a) {
+                if (i % num != 0) {
+                    continue intCheck;
+                }
+            }
+
+            for (int num : b) {
+                if (num % i != 0) {
+                    continue intCheck;
+                }
+            }
+
+            sum++;
+        }
+
+        return sum;
+    }
+	
+	static String kangaroo(int x1, int v1, int x2, int v2) {
+		int n = 20000;
+		if (x1 > x2) {
+			n = x1 * n;
+		} else {
+            n = x2 * n;
+        }
+		
+		//System.out.println(String.format("times == %d", n));
+		
+		for (int i=x1, j=x2; i<n; i += v1, j += v2) {
+			//System.out.println(String.format("%d == %d", i, j));
+			if (i == j) {
+				return "YES";
+			}
+		}
+		
+		return "NO";
+    }
+	
+	public static void appleAndOrange() {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int t = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int[] apple = new int[m];
+        for(int apple_i=0; apple_i < m; apple_i++){
+            apple[apple_i] = in.nextInt();
+        }
+        int[] orange = new int[n];
+        for(int orange_i=0; orange_i < n; orange_i++){
+            orange[orange_i] = in.nextInt();
+        }
+        
+        // code begins
+        
+        int a_arr[] = new int[apple.length];
+        for (int i=0; i<apple.length; i++) {
+            a_arr[i] = a + apple[i];
+        }
+
+        int b_arr[] = new int[orange.length];
+        for (int i=0; i<orange.length; i++) {
+            b_arr[i] = b + orange[i];
+        }
+
+        int a_sum = 0;
+        for (int i=0; i<a_arr.length; i++) {
+            if (a_arr[i] >= s && a_arr[i] <= t) {
+                a_sum += 1;
+            }
+        } 
+
+        int b_sum = 0;
+        for (int i=0; i<b_arr.length; i++) {
+            if (b_arr[i] >= s && b_arr[i] <= t) {
+                b_sum += 1;
+            }
+        }
+
+        System.out.println(a_sum);
+        System.out.println(b_sum);
+    }
 	
 	public static void testMain() throws Exception {
 		List<String> list = new ArrayList<>();
